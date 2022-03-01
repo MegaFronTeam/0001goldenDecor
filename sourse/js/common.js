6540a4e8f9184d4e3982b0febbbdd21b3b79612e
Janis <janis.paberzs18@gmail.com>
2022-03-01 13:33:08 +0200

    edit 03-catalog

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -484,6 +484,67 @@ function eventHandler() {
 	$(".dd-head-js").click(function () {
 		$(this).toggleClass("active").next().slideToggle();
 	})
+
+	var $range = $(".js-range-slider");
+	var $inputFrom = $(".js-input-from");
+	var $inputTo = $(".js-input-to");
+	var instance;
+	var min = 0;
+	var max = 20000;
+	var from = 0;
+	var to = 0;
+	
+	$range.ionRangeSlider({
+			skin: "round",
+			type: "double",
+			min: min,
+			max: max,
+			from: 890,
+			to: 18090,
+			postfix: "₽",
+			onStart: updateInputs,
+			onChange: updateInputs,
+			onFinish: updateInputs
+	});
+	instance = $range.data("ionRangeSlider");
+	
+	function updateInputs (data) {
+			from = data.from;
+			to = data.to;
+	
+			$inputFrom.prop("value", from);
+			$inputTo.prop("value", to);
+	}
+	
+	$inputFrom.on("change", function () {
+			var val = $(this).prop("value");
+			console.log(val);
+			// validate
+			if (val < min) {
+				val = min;
+			} else if (val > to) {
+				val = to;
+			}
+			instance.update({
+				from: val
+			});
+			$(this).prop("value", val);
+		});
+	
+	$inputTo.on("change", function () {
+		var val = $(this).prop("value");
+		// validate
+		if (val < from) {
+				val = from;
+		} else if (val > max) {
+				val = max;
+		}
+		instance.update({
+				to: val
+		});
+		$(this).prop("value", val);
+	});
+
 };
 if (document.readyState !== 'loading') {
 	eventHandler();
